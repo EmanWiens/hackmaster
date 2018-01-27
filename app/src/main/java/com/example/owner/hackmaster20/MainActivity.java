475763cc674c77724a4544c9205466e771112dfb
Vlad <vladsamonin@gmail.com>
2018-01-26 22:30:49 -0600

    VladIsawesome
    test

--- app/src/main/java/com/example/owner/hackmaster20/MainActivity.java
+++ app/src/main/java/com/example/owner/hackmaster20/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// comment vlad is awesome
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
