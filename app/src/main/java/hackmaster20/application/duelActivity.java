ef6b9cea8f095ab41f4d79fd3132ee6cea00beee
drfruitface <drFruitFace@github.com>
2018-01-29 00:08:49 -0600

    Version 1 of duel GUI is done

--- app/src/main/java/hackmaster20/application/duelActivity.java
+++ app/src/main/java/hackmaster20/application/duelActivity.java
@@ -10,6 +10,7 @@ public class duelActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_duel);
     }
 }
