ad32b3bdefcbc4ca362650b8444e81a07b87b514
drfruitface <drFruitFace@github.com>
2018-02-03 19:34:47 -0600

    Minor changes

--- app/src/main/java/hackmaster20/presentation/MainActivity.java
+++ app/src/main/java/hackmaster20/presentation/MainActivity.java
@@ -9,16 +9,19 @@ import android.widget.TextView;
 
 import com.example.owner.hackmaster20.R;
 
+import org.w3c.dom.Text;
+
 import hackmaster20.business.GameManager;
 import hackmaster20.objects.CardClass;
 
 public class MainActivity extends AppCompatActivity implements DrawToScreen {
     // give a "copy" of the interface to the gameManager
-    private GameManager gameManager = new GameManager(this);
+    private GameManager gameManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        gameManager = new GameManager(this);
         setContentView(R.layout.activity_main);
     }
 
@@ -41,15 +44,18 @@ public class MainActivity extends AppCompatActivity implements DrawToScreen {
         textView.setText(card.toString());
     }
 
-    public void playMessage(View v) {
+    public void singlePlayerPress(View v) {
         setContentView(R.layout.sample_my_view);
         gameManager.setUpSingleGame();
     }
 
     public void cardPress(View v) {
         if (gameManager.getPlayerTurn()) {
+            String name[] = ((TextView)v).getText().toString().split("\n");
             TextView playedCard = (TextView) findViewById(R.id.playedCard1);
             playedCard.setText(((TextView) v).getText());
+
+            gameManager.playCardEvent(name[0]);
             // TODO use this to call GameManager function to continue turn base
         }
     }
