0df34888934e99ccb6ecbf426ebbe096fed4e454
detritivore11 <35715228+detritivore11@users.noreply.github.com>
2018-03-09 22:09:25 -0600

    Linked SQL database to deckmanager

---
+++ app/src/main/java/hackmaster/application/DBController.java
@@ -0,0 +1,31 @@
+package hackmaster.application;
+
+import hackmaster.presentation.MainActivity;
+
+public class DBController
+{
+	public static final String dbName = "HM_DB";
+	private static String dbPathName = "database/HM_DB";
+
+	public static void startUp()
+	{
+		Services.createDataAccess(dbName);
+	}
+
+	public static void shutDown()
+	{
+		Services.closeDataAccess();
+	}
+
+	public static String getDBPathName() {
+		if (dbPathName == null)
+			return dbName;
+		else
+			return dbPathName;
+	}
+
+	public static void setDBPathName(String pathName) {
+		System.out.println("Setting DB path to: " + pathName);
+		dbPathName = pathName;
+	}
+}
