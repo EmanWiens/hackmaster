6b58c8b503b7cc0659a6d28ef1b4783b3ca85d8b
drfruitface <drFruitFace@github.com>
2018-03-12 18:39:32 -0500

    Deleted two lines (Github glitched)

--- app/src/main/java/hackmaster/business/DeckManager.java
+++ app/src/main/java/hackmaster/business/DeckManager.java
@@ -11,14 +11,11 @@ public abstract class DeckManager {
     private static CardClass[] deck = null;
     private static CardDataAccessInterface cardDataAccess;
 
-    abstract void methodToOverride(); // TODO example of a method to be overriden by subclass
-
     public static void initDeck() {
         cardDataAccess = Services.getCardDataAccess();
         ArrayList<CardClass>listDeck = new ArrayList<>();
         String emsg = cardDataAccess.getCardSequential(listDeck);
         deck = listDeck.toArray(new CardClass[0]);
-        //shuffleDeck();
         resetIndex();
     }
 
