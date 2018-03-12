b13b8905fe480b1b3228cfa6be4fdf37957f4709
drfruitface <drFruitFace@github.com>
2018-03-11 20:05:59 -0500

    Minor bug fix for applying card

--- app/src/main/java/hackmaster/business/SetUpGame.java
+++ app/src/main/java/hackmaster/business/SetUpGame.java
@@ -5,18 +5,18 @@ import hackmaster.objects.PlayerClass;
 import hackmaster.objects.ResourceClass;
 
 public abstract class SetUpGame {
-    public static final ResourceClass startOfGameResources = new ResourceClass(100, 10, 2, 10, 2, 10, 2);
+    private static final ResourceClass startOfGameResources = new ResourceClass(100, 10, 2, 10, 2, 10, 2);
 
     public static Game setUpSinglePlayerGame() {
         DeckManager.initDeck();
 
         PlayerClass player1 = new PlayerClass(0,
                 "HackerMan",
-                startOfGameResources, DeckManager.dealFirstHandOfGame());
+                startOfGameResources.clone(), DeckManager.dealFirstHandOfGame());
 
         PlayerClass player2 = new EnemyAI(1,
                 "Enemy Bot",
-                startOfGameResources, DeckManager.dealFirstHandOfGame());
+                startOfGameResources.clone(), DeckManager.dealFirstHandOfGame());
 
         return new SinglePlayerGame(player1, player2);
     }
@@ -26,12 +26,14 @@ public abstract class SetUpGame {
 
         PlayerClass player1 = new PlayerClass(0,
                 "HackerMan",
-                startOfGameResources, DeckManager.dealFirstHandOfGame());
+                startOfGameResources.clone(), DeckManager.dealFirstHandOfGame());
 
         PlayerClass player2 = new PlayerClass(1,
                 "HackerMan-2nd",
-                startOfGameResources, DeckManager.dealFirstHandOfGame());
+                startOfGameResources.clone(), DeckManager.dealFirstHandOfGame());
 
         return new MultiplayerGame(player1, player2);
     }
+
+    public static final ResourceClass startOfGameResources() { return startOfGameResources.clone(); }
 }
