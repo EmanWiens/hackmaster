dbcab101da1c9bf8d30f1ada3ecc365b5ebe6048
Vlad <vladsamonin@gmail.com>
2018-03-14 04:11:34 -0500

    Fixed Small issue

--- app/src/main/java/HackMaster/objects/EnemyAI.java
+++ app/src/main/java/HackMaster/objects/EnemyAI.java
@@ -24,14 +24,14 @@ public class EnemyAI extends PlayerClass {
         return nextCard;
     }
 
-    private int bestCard(CardClass[] playable) {
+    public int bestCard(CardClass[] playable) {
         int bestCard = -1;
-        int bestCost = 10000;
+        int bestCost = -1;
 
         for (int i = 0; i < playable.length; i++) {
             ResourceClass cardR = playable[i].getPlayerR();
-            int testCost = cardR.getBotnet() + cardR.gethCoin() + cardR.getCpu() + cardR.getHealth();
-            if (testCost <= bestCost) {
+            int testCost = Math.abs(cardR.getBotnet()) + Math.abs(cardR.gethCoin()) + Math.abs(cardR.getCpu()) + Math.abs(cardR.getHealth());
+            if (testCost >= bestCost) {
                 bestCost = testCost;
                 bestCard = i;
             }
